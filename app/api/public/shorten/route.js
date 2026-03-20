2d1f9f16962aa7540c105ff60046e0c26bf6d599
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2026-03-21 00:13:50 +0530

    Update route.js

--- app/api/public/shorten/route.js
+++ app/api/public/shorten/route.js
@@ -60,6 +60,6 @@ export async function GET(req) {
   // 🔥 RETURN YOUR LINK (NOT external)
   return NextResponse.json({
     status: "success",
-    shortlink: `${baseUrl}/start/${customId}`
+    shortlink: `https://securelink-network.vercel.app/start/${customId}`
   });
 }
